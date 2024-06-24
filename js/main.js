ea1085be40535bc55d0ef5f6dc3745df29abcc68
Muhammad Shafay <shafin22405@gmail.com>
2024-06-24 16:48:16 -0700

    build navbar component.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-const collapsibles = document.querySelectorAll('.collapsible');
+const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
-    item.addEventListener('click', function () {
-        this.classList.toggle('collapsible--expanded');
- })
-)
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+const navIcon = document.querySelector(".nav__icon-wrapper");
+const nav = document.querySelector(".nav");
+navIcon.addEventListener("click", function () {
+  nav.classList.toggle("nav--expanded");
+});
